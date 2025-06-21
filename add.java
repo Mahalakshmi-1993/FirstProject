1a4588f17b665eb55001ab4a900c60adf7578efc
Mahalakshmi S <r.s.maha.cse@gmail.com>
2025-06-21 10:53:12 +0530

    updated variable value as 30

--- add.java
+++ add.java
@@ -1,7 +1,7 @@
 public class Add{
     public static void main(String[] args) {
         int a = 5;
-        int b = 10;
+        int b = 30;
         int sum = a + b;
         System.out.println("The sum of " + a + " and " + b + " is: " + sum);
     }
